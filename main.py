80f15ef9fedd520908091c33d3169a7fb0caae2c
Minna Soudunsaari <117842115+TheMinna@users.noreply.github.com>
2023-02-24 14:13:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from turtle import Turtle, Screen
 import random
-from car import Car, Bus
+from car import Car
 from froggy import Froggy
 from scoreboard import Scoreboard
 import turtle
 import time
+
 
 
 screen = Screen()
@@ -23,25 +24,17 @@ screen.listen()
 
 scoreboard = Scoreboard()
 
+#### car list moved to car.py:
+# car = Car()
+# car.shape(random.choice(car_pics))
+# #car.resizemode('user')
 
-car_pics = ['car_small.gif', 'car2_small.gif', 'car3_small.gif']
-bus_pics = ['bus_small.gif', 'bus2_small.gif' , 'bus3_small.gif']
+car_manager = Car()
 
-car = Car()
-car.shape(random.choice(car_pics))
-#car.resizemode('user')
-
-# car_number = 10
-# cars = {num: Car() for num in range(car_number)}
-
-# for car in cars:
-#     print(car)
-#     #car.shape(random.choice(car_pics))
-
-
-bus = Bus()
-bus.shape(random.choice(bus_pics))
-#bus.resizemode('user')
+# bus_pics = ['bus_small.gif', 'bus2_small.gif' , 'bus3_small.gif']
+# bus = Bus()
+# bus.shape(random.choice(bus_pics))
+# #bus.resizemode('user')
 
 froggy = Froggy()
 
@@ -56,21 +49,22 @@ game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
-    car.move()
-    bus.move()
-    #print(f"froggy y : {froggy.ycor()}")
+    car_manager.create_vehicle()
+    car_manager.move_cars()
 
-    if froggy.distance(car) < 35 and froggy.ycor() == car.ycor():
-        scoreboard.game_over()
-        game_on = False
+    for car in car_manager.all_vehicles:
+        if froggy.distance(car) < 35 and froggy.ycor() == car.ycor():
+            scoreboard.game_over()
+            game_on = False
     
-    if froggy.distance(bus) < 55 and froggy.ycor() == bus.ycor():
-        scoreboard.game_over()
-        game_on = False
+    # if froggy.distance(bus) < 55 and froggy.ycor() == bus.ycor():
+    #     scoreboard.game_over()
+    #     game_on = False
 
     if froggy.ycor() >= 280:
-        froggy.restart_level()
         scoreboard.increase_score()
+        froggy.restart_level()
+        car_manager.level_up()
         
 
 screen.mainloop()
