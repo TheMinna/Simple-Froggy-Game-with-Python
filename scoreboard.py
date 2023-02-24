41101ddc53b32b8cf1276afe397aa00f2a0376f9
Minna Soudunsaari <117842115+TheMinna@users.noreply.github.com>
2023-02-24 14:13:23 +0100

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 
 FONT = ('Courier', 14, 'bold')
 
+
 class Scoreboard(Turtle):
 
     def __init__(self):
@@ -12,7 +13,7 @@ class Scoreboard(Turtle):
         self.penup()
         self.setpos(-250, 270)
         self.update_scoreboard()
-    
+
     def game_over(self):
         self.goto(0, 0)
         self.write(f"GAME OVER", move=False, align='center', font=FONT)
